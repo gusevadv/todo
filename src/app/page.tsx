5d7d05fbe71ac80a80c236f02eb58832ea5d02d2
gusevadv <dapril310@gmail.com>
2023-02-24 14:28:17 +0700

    Refactor state to reducers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer, useCallback } from 'react'
 
 type Todo = {
   id: number
@@ -16,6 +16,7 @@ const defaultTodos: Todo[] = [
 ]
 
 let index = (defaultTodos.at(-1)?.id ?? -1) + 1
+let checkedFilter = false
 
 function getNewTodoItem() {
   const newTodo: Todo = {
@@ -27,50 +28,133 @@ function getNewTodoItem() {
   return newTodo
 }
 
+function filterTodos(todos: Todo[], completed: boolean): Todo[] {
+  const filteredTodos = completed ? todos.filter(todo => todo.checked === true) : todos
+  return filteredTodos
+}
+
+function getCounter(todos: Todo[]): string {
+  const allItem = todos.length
+  const trueCheckedItem = todos.filter((todo) => todo.checked === true)
+  return trueCheckedItem.length + '/' + allItem
+}
+
+type State = {
+  todos: Todo[]
+  filteredTodos: Todo[]
+  completed: boolean
+  counter: string
+}
+
+type Action =
+  | { type: 'ADD_TODO' }
+  | { type: 'UPDATE_TODO', payload: { todo: Todo } }
+  | { type: 'DELETE_TODO', payload: { todo: Todo } }
+  | { type: 'FILTER_TODOS', payload: { completed: boolean } }
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      const newTodo = getNewTodoItem()
+      const newTodos = [...state.todos, newTodo]
+      const filteredTodos = filterTodos(newTodos, state.completed)
+      const counter = getCounter(newTodos)
+      return {
+        ...state,
+        todos: newTodos,
+        filteredTodos,
+        counter
+      }
+    }
+    case 'UPDATE_TODO': {
+      const updatedTodo = action.payload.todo
+      const updatedIndex = state.todos.findIndex((todo) => todo.id === updatedTodo.id)
+      const updatedTodos = [...state.todos]
+      updatedTodos.splice(updatedIndex, 1, updatedTodo)
+      const filteredTodos = filterTodos(updatedTodos, state.completed)
+      const counter = getCounter(updatedTodos)
+      return {
+        ...state,
+        todos: updatedTodos,
+        filteredTodos,
+        counter
+      }
+    }
+    case 'DELETE_TODO': {
+      const deletedTodo = action.payload.todo
+      const deleteIndex = state.todos.findIndex((todo) => todo.id === deletedTodo.id)
+      const updatedTodos = [...state.todos]
+      updatedTodos.splice(deleteIndex, 1)
+      const filteredTodos = filterTodos(updatedTodos, state.completed)
+      const counter = getCounter(updatedTodos)
+      return {
+        ...state,
+        todos: updatedTodos,
+        filteredTodos,
+        counter
+      }
+    }
+    case 'FILTER_TODOS': {
+      const completed = action.payload.completed
+      const filteredTodos = filterTodos(state.todos, completed)
+      const counter = getCounter(state.todos)
+      return {
+        ...state,
+        filteredTodos,
+        completed,
+        counter
+      }
+    }
+    default:
+      return state
+  }
+}
+
+const initialState: State = {
+  todos: defaultTodos,
+  filteredTodos: filterTodos(defaultTodos, false),
+  completed: false,
+  counter: getCounter(defaultTodos)
+}
+
 export default function Page() {
-  const [todos, setTodos] = useState(defaultTodos)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const handleAddItem = () => {
-    const newTodo = getNewTodoItem()
-    const newTodos = [...todos, newTodo]
-    setTodos(newTodos)
-
-    // setTodos(oldTodos => {
-    //   return [...oldTodos, newtodo]
-    // })
+    dispatch({ type: 'ADD_TODO' })
   }
 
-  const handleUpdateItem = (updatedTodo: Todo) => {
-    const updatedIndex = todos.findIndex((todo) => todo.id === updatedTodo.id)
-    const updatedTodos = [...todos]
-    updatedTodos.splice(updatedIndex, 1, updatedTodo)
-    setTodos(updatedTodos)
+  const handleUpdateItem = (todo: Todo) => {
+    dispatch({ type: 'UPDATE_TODO', payload: { todo }})
   }
 
-  const handleDeleteItem = (deleteTodo: Todo) => {
-    const deleteIndex = todos.findIndex((todo) => todo.id === deleteTodo.id)
-    const updatedTodos = [...todos]
-    updatedTodos.splice(deleteIndex, 1)
-    setTodos(updatedTodos)
+  const handleDeleteItem = (todo: Todo) => {
+    dispatch({ type: 'DELETE_TODO', payload: { todo }})
   }
+
+  const handleFilterTodo = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const completed = event.target.checked
+    dispatch({ type: 'FILTER_TODOS', payload: { completed }})
+  }
+
   return (
     <main >
       <div className="container mx-auto flex flex-col shadow-lg w-96 rounded-2xl bg-white p-5 gap-3">
         <div className="flex flex-row justify-between gap-6 place-content-center">
           <div className="flex flex-row gap-2">
-            <div>
-            0/2
+            <div className="text-[#818181]">
+              {state.counter}
             </div>
             <div>
              Shoping List
             </div>
           </div>
           <div >
-            <input className="m-1" type="checkbox" />Completed
+            <input className="m-1" type="checkbox" checked={state.completed} onChange={handleFilterTodo}/>Completed
           </div>
         </div>
         <TodoList >
-          {todos.map(todo => <TodoItem key={todo.id} item={todo} onChange={handleUpdateItem} onDelete={handleDeleteItem}/>)}
+          {state.filteredTodos.map(todo => <TodoItem key={todo.id} item={todo} onChange={handleUpdateItem} onDelete={handleDeleteItem}/>)}
         </TodoList>
         <button className="bg-gray-500 self-end" onClick={handleAddItem}>+ Add an item</button>
       </div>
